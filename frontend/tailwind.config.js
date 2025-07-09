c6cff3f610d568c4019b51a851f2ba3fec2b48fd
Allen Huang <allen2990167@gmail.com>
2025-07-08 21:42:49 -0700

    2 - UI Design Completed

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,29 @@
+import daisyui from "daisyui";
+// import daisyUIThemes from "daisyui/src/theming/themes";
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
+
+	daisyui: {
+		themes: [
+			"light",
+			{
+				black: {
+						"primary": "rgb(29, 155, 240)",
+						"secondary": "rgb(24, 24, 24)",
+						"accent": "#37CDBE",
+						"neutral": "#3D4451",
+						"base-100": "#000000",
+						"info": "#3ABFF8",
+						"success": "#36D399",
+						"warning": "#FBBD23",
+						"error": "#F87272",
+				},
+			},
+		],
+	},
+};
